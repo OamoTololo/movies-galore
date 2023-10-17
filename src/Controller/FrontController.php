82e74a127ef1fae515cc82d4beeb59d09c308914
Oamogetswe Mgidi <OamogetsweM@miway.co.za>
2023-10-18 00:46:11 +0200

    removed php tags and added twig tags for files in the template folder and added a front controller.

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -9,10 +9,66 @@ use Symfony\Component\Routing\Annotation\Route;
 class FrontController extends AbstractController
 {
     /**
-     * @Route("/front", name="app_front")
+     * @Route("/", name="app_front")
      */
     public function index(): Response
     {
         return $this->render('front/index.html.twig');
+    }
+
+    /**
+     * @Route("/video-list", name="app_video_list")
+     */
+    public function videoList(): Response
+    {
+        return $this->render('front/video_list.html.twig');
+    }
+
+    /**
+     * @Route("/video-details", name="app_video_details")
+     */
+    public function videoDetails(): Response
+    {
+        return $this->render('front/video_details.html.twig');
+    }
+
+    /**
+     * @Route("/search-results", name="app_search_results")
+     */
+    public function searchResults(): Response
+    {
+        return $this->render('front/search_results.html.twig');
+    }
+
+    /**
+     * @Route("/register", name="app_register")
+     */
+    public function register(): Response
+    {
+        return $this->render('front/register.html.twig');
+    }
+
+    /**
+     * @Route("/pricing", name="app_pricing")
+     */
+    public function pricing(): Response
+    {
+        return $this->render('front/pricing.html.twig');
+    }
+
+    /**
+     * @Route("/payment", name="app_payment")
+     */
+    public function payment(): Response
+    {
+        return $this->render('front/payment.html.twig');
+    }
+
+    /**
+     * @Route("/login", name="app_login")
+     */
+    public function login(): Response
+    {
+        return $this->render('front/login.html.twig');
     }
 }
