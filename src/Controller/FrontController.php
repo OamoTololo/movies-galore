875375b6ab8702e63d940c17889bc932ab89d194
Oamogetswe Mgidi <OamogetsweM@miway.co.za>
2023-10-24 07:29:24 +0200

    Finished off displaying categories on front page.

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -23,10 +23,9 @@ class FrontController extends AbstractController
      */
     public function videoList($id, CategoryTreeFrontPage $categories): Response
     {
-        $subcategories = $categories->buildTree($id);
-        dump($subcategories);
+        $categories->getCategoryListAndParent($id);
         return $this->render('front/video_list.html.twig',[
-            'subcategories' => $categories->getCategoryList($subcategories)
+            'subcategories' => $categories
         ]);
     }
 
